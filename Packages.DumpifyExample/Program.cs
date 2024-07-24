40b794bdb6a572d07bbb284f335f6110d63783de
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-24 17:35:19 +0630

    Add project files.

---
+++ Packages.DumpifyExample/Program.cs
@@ -0,0 +1,31 @@
+﻿using Dumpify;
+using Packages.DumpifyExample;
+using System.Reflection.Metadata;
+
+new { Name = "Dumpify", Description = "Dump any object to Console" }.Dump();
+
+var blog = new BlogModel()
+{
+    BlogId = Guid.NewGuid().ToString(),
+    BlogTitle = "Dumpify",
+    BlogAuthor = "Dumpify",
+    BlogContent = "Dumpify"
+};
+
+blog.Dump();
+
+var lst = Enumerable.Range(1, 3).Select(x =>
+{
+    BlogModel blog = new BlogModel()
+    {
+        BlogId = Guid.NewGuid().ToString(),
+        BlogTitle = "Test Title",
+        BlogAuthor = "Test Author",
+        BlogContent = "Test Content"
+    };
+
+    return blog;
+});
+lst.Dump();
+
+Console.ReadKey();
